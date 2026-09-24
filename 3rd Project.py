01b3a3087e4c21438990df8ec349591acef7aece
Wasay Basit <wasaybasit14@gmail.com>
2026-09-24 23:05:56 +0500

    Add personal expense report

---
+++ 3rd Project.py
@@ -0,0 +1,115 @@
+Name = 'Syed Wasay Basit'
+Age = 17
+Date = '24th September 2026'
+Occupation = 'Student'
+print (Name)
+print (Age)
+print (Date)
+print (Occupation)
+# Syed Wasay Basit's 3rd Project
+# Personal Expense Report
+Food = 540
+Transport = 400
+Football = 950
+Games = 1000
+Other = 250
+print (Food)
+print (Transport)
+print (Football)
+print (Games)
+print (Other)
+Total_expenses = Food + Transport + Football + Games + Other
+Subtracted_amount_between_games_and_food = Games - Food
+Doubled_football_budget = Football *2
+Average_expense = Total_expenses / 5
+Remaining_money = 628
+Module_of_remaining_money = Remaining_money % 10
+Floor_division_of_transport_and_football = Football // Transport
+Exponent_of_Total_expenses = Total_expenses **2
+print (Total_expenses)
+print (Subtracted_amount_between_games_and_food)
+print (Doubled_football_budget)
+print (Average_expense)
+print (Module_of_remaining_money)
+print (Floor_division_of_transport_and_football)
+print (Exponent_of_Total_expenses)
+my_str = 'A boy trying to find his way through the world filled with competition of technological skills.'
+uppered_my_str = my_str.upper()
+print (uppered_my_str)
+my_str = 'This Personal Expense Report is made by a 17 year old boy, Wasay Basit.'
+lowered_my_str = my_str.islower()
+print (lowered_my_str)
+my_str = 'Red Dead Redemption 2 is dope than Fortnite.'
+replaced_my_str = my_str.replace('Fortnite.', 'God Of War.')
+print (replaced_my_str)
+my_str = 'Hello People.'
+splitted_my_str = my_str.split()
+print (splitted_my_str)
+my_list = ['Hello', 'People']
+joined_my_words = ' '.join(my_list)
+print (joined_my_words)
+my_str = 'Six Seven'
+starts_with_str = my_str.startswith('Six')
+print (starts_with_str)
+my_str = 'Python and CSS'
+ends_with_str = my_str.endswith('CSS')
+print (ends_with_str)
+my_str = 'Q08N86QH75Zz8873n'
+find_my_str = my_str.find('Q')
+print (find_my_str)
+my_str = 'MJQQT421'
+count_my_str = my_str.count('Q')
+print (count_my_str)
+my_str = 'A Cybersecurity specialist establishes a survey.'
+capitalized_my_str = my_str.capitalize()
+print (capitalized_my_str)
+my_str = 'AI tools exist to make our softwares run easily and efficiently.'
+titled_my_str = my_str.title()
+print (titled_my_str)
+my_str = 'A hyperactive rabbit jumped over a lazy fox.'
+print ('lazy' in my_str)
+print ('b' in my_str)
+print ('u' in my_str)
+my_str = "On a quiet Tuesday evening, a curious programmer sat beside the window with a cup of tea, wondering whether a tiny Python script could somehow turn an ordinary night into the beginning of an unexpectedly exciting adventure."
+print (my_str[1:19:2])
+print (my_str[::9])
+print (my_str[1:-11])
+print (my_str[15::])
+int_1 = 500
+int_2 = 500
+int_3 = 1000
+int_4 = 50
+int_5 = 100
+int_6 = 2000
+my_str = f'The sum of {int_1} and {int_2} turns into {int_3} and then it gets divided by {int_4} and mutiples by {int_5} so the final answer will be {int_6}'
+print (my_str)
+my_str_1 = 'Syed'
+my_str_2 = 'Wasay'
+my_str_3 = 'Basit'
+sum_of_strings = my_str_1 + ' ' + my_str_2 + ' ' + my_str_3
+print (sum_of_strings)
+integer = 170
+print(type(integer))
+float_number = 1.5
+print (type(float))
+string = 'VS Code'
+print (type(string))
+boolean = True
+print (type(boolean))
+integer = 76
+conversion_of_integer = float(integer)
+print (conversion_of_integer)
+string = '45'
+conversion_of_string = int('45')
+print (conversion_of_string)
+string = '7.5'
+conversion_of_string = float(string)
+print (conversion_of_string)
+integer = 1987
+print (isinstance(integer, (int,float)))
+
+# This is the end of my Project 3.
+# Thank you for viewing my 3rd Project!
+# Have a good day, Y'all!
+
+
